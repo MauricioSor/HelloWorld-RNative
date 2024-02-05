e20d95851f3b57636417cd0937417a284a6137e5
Mauricio Soria <mauricioutn2017@gmail.com>
2024-02-05 16:41:55 -0300

    Creada pantalla de inicio

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image, ViewBase } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen.jsx';
-
+import Inicio from "./assets/logo.png"
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [bienvenido, setBienvenido] = useState(true)
+
+useEffect(() => {
+  setTimeout(() => {
+    bienvenida();
+  }, 2000);
+},[])
+const bienvenida =()=>{
+setBienvenido(false)
+}
+if(bienvenido==true){
+  return(
+    <View style={styles.inicio}>
+    <Image source={Inicio} />
+    </View>
+  )
+}
   return (
+    
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Puto el que leEASDAWE</Text>
-      <HomeScreen></HomeScreen>
+      <HomeScreen/>
     </View>
   );
 }
@@ -21,4 +40,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inicio:{
+    flex:1,
+    backgroundColor:"black",    
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
